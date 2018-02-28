5bffd31504726e5b3ea49e1ce83336747fdfa841
tss <tss@debian-tss24>
2018-02-28 08:48:15 +0100

    gestore client server

---
+++ src/main/java/tss/it/mavenproject1/EcoClient.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tss.it.mavenproject1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author tss
+ */
+
+
+public class EcoClient {
+static String tx;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+
+        Socket s = new Socket("192.168.40.213", 21000); //192.168.40.213  "192.168.40.212"
+    
+      
+       
+        //creo un pacchetto di output
+         OutputStream outputStream = s.getOutputStream();
+         PrintWriter out = new PrintWriter(outputStream, true);
+        //riempio il pacchetto di output
+      do{
+         tx = JOptionPane.showInputDialog("cosa vuoi scrivere al server?");
+         out.println(tx);
+         
+          //creo pacchetto di input
+         InputStream is = s.getInputStream(); 
+         BufferedReader bf = new BufferedReader(new InputStreamReader(is));
+         String comandoRx = bf.readLine();
+        //stampo pacchetto di input    
+         System.out.println(comandoRx);
+         
+         
+       }while(!tx.equals("fine"));
+       
+         
+        
+         
+       
+   
+   
+        
+       
+       
+       /* InputStream is = s.getInputStream(); 
+        BufferedReader bf = new BufferedReader(new InputStreamReader(is));
+        String comandoRx = bf.readLine();
+       System.out.println(comandoRx);
+         */
+        
+    
+    }
+}
+
---
+++ src/main/java/tss/it/mavenproject1/EcoServer.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tss.it.mavenproject1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author tss
+ */
+public class EcoServer {
+static String comandoRx;
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    // fare in modo che il server risponda al client facendo in continuazione l'eco dell'imput dato fino alla scrittura della parola "fine" 
+    public static void main(String[] args) throws IOException {
+        System.out.println("Server started");
+        //InetAddress addr=InetAddress.getByName("192.168.40.212);   //192.168.40.213
+        ServerSocket srvSocket = new ServerSocket(21000);
+       do { 
+            System.out.println("Waiting connection...");
+            Socket s = srvSocket.accept(); // spinotto collegamneto con client 
+            System.out.println("Connection accepted...");
+ 
+            InputStream is = s.getInputStream();
+            BufferedReader bf = new BufferedReader(new InputStreamReader(is));
+            comandoRx = bf.readLine();
+            System.out.println(comandoRx);
+
+            
+            OutputStream outputStream = s.getOutputStream();
+            PrintWriter w = new PrintWriter(outputStream, true);
+            
+            w.println(comandoRx +"   "+ comandoRx );
+
+            if(comandoRx.equals("fine")) {
+
+                s.close();
+                System.out.println("connection ended...");}
+            
+
+ }while(true); 
+ 
+ 
+ 
+ 
+    }
+
+}
